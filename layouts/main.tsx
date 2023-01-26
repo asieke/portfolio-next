d88b13932f6bae913ee9f52c4462c29b22e099dc
Alex Sieke <asieke@gmail.com>
2023-01-26 16:15:17 -0500

    adds a new privacy policy page, a logo image, and styles for the About page.
    • Added a new privacy policy page
    • Added a logo image to the public folder
    • Imported useRouter from next/router for pathname console log
    • Added conditionals to show/hide the sidebar based on pathname
    • Added styles for the About page

--- layouts/main.tsx
+++ layouts/main.tsx
@@ -1,19 +1,33 @@
 //create a type for children
 import Sidebar from '@/components/Sidebar';
 import Header from '@/components/Header';
+import { useRouter } from 'next/router';
 
 type LayoutProps = {
   children: React.ReactNode;
 };
 
 const MainLayout = ({ children }: LayoutProps) => {
+  //console.log the current path using next.js pattern
+
+  const router = useRouter();
+  const { pathname } = router;
+
+  let showSidebar = true;
+  //if pathname contains privacy then do not show sidebar
+  if (pathname.includes('privacy')) {
+    showSidebar = false;
+  }
+
   return (
-    <div className='flex flex-col h-screen'>
+    <div className='flex flex-col min-h-screen'>
       <Header />
       <div className='flex flex-row h-full'>
-        <div id='sidebar' className='w-48	hidden md:block bg-gray-600'>
-          <Sidebar />
-        </div>
+        {showSidebar && (
+          <div id='sidebar' className='w-48	hidden md:block bg-gray-600'>
+            <Sidebar />
+          </div>
+        )}
         <div className='grow bg-gray-200 p-6'>{children}</div>
       </div>
     </div>
---
+++ pages/about/privacy.tsx
@@ -0,0 +1,266 @@
+//create and export an about component
+
+const Privacy = () => {
+  return (
+    <div className='privacy-policy w-full md:w-1/2 sm:w-4/5 mx-auto'>
+      <h2>I. Privacy Policy</h2>
+      <h4>Last Updated: January 26, 2023</h4>
+      <p>
+        This privacy policy (&quot;Policy&quot;) describes how predict.exchange
+        (&quot;predict.exchange&quot;, &quot;we&quot;, &quot;us&quot; or &quot;our&quot;) collects,
+        protects and uses the personally identifiable information (&quot;Personal Information&quot;)
+        you (&quot;User&quot;, &quot;you&quot; or &quot;your&quot;) may provide through the
+        predict.exchange website (predict.exchange) or in the course of purchasing any
+        predict.exchange products (collectively, &quot;Website&quot;). The Policy also describes the
+        choices available to you regarding our use of your Personal Information and how you can
+        access and update this information. This Policy does not apply to the practices of companies
+        that we do not own or control, or to individuals that we do not employ or manage.
+      </p>
+      <h1>Introduction</h1>
+      <h3>A. Purpose of the privacy policy</h3>
+      <p>
+        The purpose of this privacy policy is to inform users of the website about how their
+        personal information is collected, used, and protected. We are committed to ensuring the
+        privacy and security of our users&apos; information.
+      </p>
+      <h3>B. Description of the website and its services</h3>
+      <p>
+        Our website connects a user&apos;s financial accounts into a single dashboard and gives them
+        insights into how they can better manage their assets, tax loss harvest, direct index, and
+        get investment insights. By connecting their financial accounts, users can view all of their
+        financial information in one place, making it easier to track their investments and make
+        informed decisions.
+      </p>
+      <h3>C. Types of personal information collected</h3>
+      <p>
+        In order to provide our services, we collect certain personal information from our users.
+        This includes, but is not limited to, information such as name, email address, financial
+        account information, and login credentials. This information is used to connect the
+        user&apos;s financial accounts, provide personalized investment insights, and to communicate
+        with the user about our services.
+      </p>
+
+      <h1>II. Definitions</h1>
+      <h3>a. Personal Information</h3>
+      <p>
+        Personal information refers to any information that can be used to identify an individual,
+        including but not limited to name, email address, financial account information, and login
+        credentials.
+      </p>
+      <h3>b. User</h3>
+      <p>A user refers to any individual who uses the website and its services.</p>
+      <h3>c. Data Controller</h3>
+      <p>
+        A data controller refers to the entity that determines the purposes and means of processing
+        personal information.
+      </p>
+
+      <h1>III. Data Collection</h1>
+      <h3>a. Types of information collected</h3>
+      <p>
+        Our website collects a variety of personal information from users in order to provide them
+        with the best possible experience and insights. This information includes, but is not
+        limited to, the following:
+      </p>
+      <ul>
+        <li>
+          Financial account information such as account balances, transactions, and investments.
+        </li>
+        <li>Demographic information such as age, income, and occupation.</li>
+        <li>Information related to the user&apos;s investment goals and risk tolerance.</li>
+        <li>Contact information such as name, email address, and phone number.</li>
+      </ul>
+      <h3>b. How information is collected</h3>
+      <p>We collect personal information from users in several ways, including:</p>
+      <ul>
+        <li>When users sign up for an account on our website.</li>
+        <li>When users connect their financial accounts to our platform.</li>
+        <li>
+          When users provide information through forms on our website, such as contact forms or
+          surveys.
+        </li>
+        <li>
+          Through cookies and other tracking technologies when users interact with our website.
+        </li>
+      </ul>
+      <h3>c. How information is safeguarded</h3>
+      <p>
+        We take the protection of personal information very seriously and have implemented a variety
+        of security measures to safeguard this information. These measures include:
+      </p>
+      <ul>
+        <li>Encryption of personal information during transmission and storage.</li>
+        <li>
+          Implementation of firewalls and other security technologies to prevent unauthorized
+          access.
+        </li>
+        <li>
+          Restrictions on access to personal information to only authorized employees and
+          contractors.
+        </li>
+        <li>
+          Regular security audits and assessments to identify and address potential vulnerabilities.
+        </li>
+      </ul>
+      <p>
+        Please be aware that despite our best efforts, no security measures are perfect or
+        impenetrable. We cannot guarantee the security of personal information and will not be
+        liable for any unauthorized access or use of personal information.
+      </p>
+
+      <h1>IV. Use of Information</h1>
+      <h3>a. How information is used</h3>
+      <p>
+        Personal information collected from users of our website is used to provide and improve our
+        services. We may use your personal information to:
+      </p>
+      <ul>
+        <li>Provide access to our dashboard and the insights it offers</li>
+        <li>Provide customer support and troubleshoot issues</li>
+        <li>Improve our website and services</li>
+        <li>
+          Communicate with you regarding updates, promotions, and other information related to our
+          services
+        </li>
+      </ul>
+      <h3>b. Who information is shared with</h3>
+      <p>We may share your personal information with:</p>
+      <ul>
+        <li>
+          Third-party service providers who assist us in providing our services, such as payment
+          processors or data analytics providers
+        </li>
+        <li>Government authorities or other third parties as required by law</li>
+      </ul>
+      <h3>c. Who information is not shared with</h3>
+      <p>
+        We will not share your personal information with any third parties for their own marketing
+        or promotional purposes without your explicit consent.
+      </p>
+      <p>We will not sell or rent your personal information to third parties.</p>
+
+      <h1>V. User Rights</h1>
+      <h3>a. Accessing Personal Information</h3>
+      <p>
+        Our company respects the user&apos;s right to access their personal information. If a user
+        would like to access their personal information, they can do so by contacting us at [insert
+        contact information here]. We will provide the user with a copy of their personal
+        information within a reasonable timeframe.
+      </p>
+      <h3>b. Making Changes to Personal Information</h3>
+      <p>
+        Our company respects the user&apos;s right to make changes to their personal information. If
+        a user would like to make changes to their personal information, they can do so by
+        contacting us at [insert contact information here]. We will make the necessary changes
+        within a reasonable timeframe.
+      </p>
+      <h3>c. Opting Out of Data Collection</h3>
+      <p>
+        Our company respects the user&apos;s right to opt out of data collection. If a user would
+        like to opt out of data collection, they can do so by contacting us at [insert contact
+        information here]. We will implement the opt-out request within a reasonable timeframe.
+      </p>
+
+      <h1>VI. Data Retention</h1>
+      <h3>a. How long information is retained</h3>
+      <p>
+        The personal information collected from users will be retained for as long as necessary to
+        provide the services requested by the user, or as required by applicable laws. This includes
+        information such as name, email address, financial account information, and login
+        credentials. Once the user terminates their account, or requests that their information be
+        deleted, we will take reasonable steps to delete the information from our systems.
+      </p>
+      <h3>b. How information is disposed of</h3>
+      <p>
+        When disposing of personal information, we will take reasonable measures to protect against
+        unauthorized access to or use of the information in transit or in storage. This may include
+        the use of secure shredding of papers containing personal information, and the secure wiping
+        of electronic media. We will also take steps to verify that the information has been
+        properly disposed of.
+      </p>
+
+      <h1>VII. Security</h1>
+      <h3>a. Description of security measures</h3>
+      <p>
+        The security of our user&apos;s personal information is of the utmost importance to us. We
+        have implemented a variety of technical and organizational measures to protect user&apos;s
+        personal information from unauthorized access, use, alteration, or disclosure. These include
+        but are not limited to:
+      </p>
+      <ul>
+        <li>Encryption of sensitive personal information during transmission and storage</li>
+        <li>Regular monitoring and testing of our security systems and procedures</li>
+        <li>
+          Restrictions on access to personal information to only authorized employees and
+          contractors who need access to perform their job duties
+        </li>
+      </ul>
+      <p>
+        In addition, we require all third-party service providers with access to our user&apos;s
+        personal information to implement appropriate security measures and comply with all
+        applicable laws and regulations.
+      </p>
+      <h3>b. Breach notification</h3>
+      <p>
+        In the event of a data breach, we will promptly notify affected users and any applicable
+        regulatory bodies in accordance with all applicable laws and regulations. We will also take
+        appropriate steps to investigate the breach, take measures to remediate the situation and
+        prevent similar incidents from occurring in the future.
+      </p>
+
+      <h1>VIII. Third-Party Links</h1>
+      <h3>a. Description of Third-Party Links</h3>
+      <p>
+        Our website uses Plaid to connect users&apos; financial accounts to our platform. By using
+        our website, you agree to the sharing of your financial account information with Plaid for
+        the purpose of linking your accounts to our platform. This information includes, but is not
+        limited to, account numbers, account balances, transaction history, and other financial
+        data.
+      </p>
+      <h3>b. Responsibility for Third-Party Links</h3>
+      <p>
+        We are not responsible for the privacy practices of any third-party links or services that
+        may be accessed through our website. We encourage you to read the privacy policies of any
+        third-party websites or services before providing them with any personal information.
+        Additionally, we do not endorse, support, or guarantee the information, products, or
+        services provided by any third-party links or services.
+      </p>
+
+      <h1> IX. Changes to Privacy Policy </h1>
+      <h3>a. How changes to Privacy Policy are communicated</h3>
+      <p>
+        We may update this Privacy Policy from time to time to reflect changes in our practices or
+        services. If we make any material changes to this Privacy Policy, we will provide notice of
+        such changes, such as by prominently posting a notice of the changes on our website or
+        sending an email notification to our users. We encourage you to review this Privacy Policy
+        periodically to stay informed about our collection, use, and disclosure of your personal
+        information.
+      </p>
+      <h3>b. Responsibility for updated Privacy Policy</h3>
+      <p>
+        By accessing or using our website or services after we have posted a notice of changes to
+        this Privacy Policy or have provided you with notice of such changes, you are deemed to have
+        accepted the changes. If you do not agree to the changes, you should discontinue your use of
+        the website or services.
+      </p>
+
+      <h1>X. Contact Information</h1>
+      <h3>a. How to contact website with questions or complaints</h3>
+      <p>
+        If you have any questions or concerns about this privacy policy or the way in which we
+        handle your personal information, please contact us at:
+      </p>
+      <p>Email: privacy@predict.exchange</p>
+      <p>Phone: 281-330-8004</p>
+      <p>
+        We will make every effort to respond to your inquiry in a timely and professional manner.
+      </p>
+      <p>
+        If you are not satisfied with our response, or if you believe that we have not adhered to
+        this privacy policy, you may file a complaint with the relevant supervisory authority.
+      </p>
+    </div>
+  );
+};
+
+export default Privacy;
